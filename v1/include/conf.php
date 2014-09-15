f218da2cd0edfb62f532d88d85cb6d89f7397c05
rosyswapna <rosy.swapna@gmail.com>
2014-09-15 15:11:49 +0530

    new constants added

---
+++ v1/include/conf.php
@@ -0,0 +1,19 @@
+<?php
+// prevent execution of this page by direct call by browser
+if ( !defined('CHECK_INCLUDED') ){
+    exit();
+}
+
+// Mysql Configuration Constants
+
+define('MYSQL_USERNAME', 'root');
+define('MYSQL_PASSWORD', 'mysql@local');
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DB_NAME', 'connectncabs');
+
+
+define('WALKIN_CUSTOMER','1');
+define('APP_REGISTRATION','2');
+define('ORG_CNC','1');
+
+?>
